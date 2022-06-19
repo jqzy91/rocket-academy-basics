e1b0f8915b16f6bad18652ad9c55f46b7fbf254e
jqzy91 <jamesquek91@live.com.sg>
2022-06-20 00:41:54 +0800

    wip blackjack

--- projects/basic-black-jack-v2/script.js
+++ projects/basic-black-jack-v2/script.js
@@ -4,20 +4,6 @@
 
 // 1. shuffling of deck
 
-// create another array to be the deck holding shuffled cards
-
-// create variables to hold player and computer card hands
-
-// create an array consisting of 6 decks -> 52 * 6 = 312 cards
-
-// create a random generator function for the shuffle function
-
-// create a shuffle function that diminishes "cards" from the cardCounter every loop run
-
-// reset cardCounter back to 312;
-
-// shuffle on page load.
-
 // 2. user clicks submit to deal cards
 // When user click submit, the program gives the player 1 card facing up, then 1 card for the computer face down. The player then receives another card, face up, and then another for the dealer, face up.
 
@@ -37,14 +23,32 @@
 
 // -------------------------------------------------
 
-// 1. Game initialization
-// - create const for suit and value.
+// 0. Pre-game initialization
+// ask for player's name. (main function)
+// when user inputs name, welcome the player.
+// there will be two buttons, "Start" and "Rules".
+// "Rules" button will explain the rules of the game.
+// "Start" button will change the game mode to gamePrepare.
+// in gamePrepare mode, "Play" and "Add Credits" button will appear
+// If there are no credits in the player's account, the "Play" button is disabled.
+// the player will be asked to enter a credit amount for waging. (max credit of 1,000)
+// when user added credits, enable the "Play" button.
+// "Play" button will change the game mode to "gameStart"
+// the player will be able to input a wager amount
+// a "Deal" button will appear next to the wager input
+// "Deal" button will initiate the 2.
 
+// 1. Game initialization
+// - set various game modes.
+let gameMode = "getUserInfo";
+
+// - give value to face cards.
 let ace = 11;
 let jack = 10;
 let queen = 10;
 let king = 10;
 
+// - create const for suit, value and name.
 const SUITS = ["Clubs", "Diamonds", "Hearts", "Spades"];
 const VALUE = [ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king];
 const NAME = [
@@ -63,24 +67,45 @@ const NAME = [
   "King",
 ];
 
-const standardDeck = [];
+// - create holder for unshuffled deck and a shuffled deck.
+const unshuffledDeck = [];
+const shuffledDeck = [];
 
-// - generate a deck using object constructors
+// - create a function to generate a deck with the help of objects
 let createDeck = function () {
-  // first loop for suits
-  for (let i = 0; i < 4; i++) {
-    // second loop for adding value and names to cards
-    for (let o = 0; o < 13; o++) {
-      standardDeck.push({
-        suit: SUITS[i],
-        value: VALUE[o],
-        name: `${NAME[o]} of ${SUITS[i]}`,
-      });
+  // first loop for 6 decks (casino standard)
+  for (let u = 0; u < 6; u++) {
+    // second loop for suits
+    for (let i = 0; i < 4; i++) {
+      // third loop for adding value and names to cards
+      for (let o = 0; o < 13; o++) {
+        unshuffledDeck.push({
+          suit: SUITS[i],
+          value: VALUE[o],
+          name: `${NAME[o]} of ${SUITS[i]}`,
+        });
+      }
     }
   }
 };
 
+// create a function to shuffle the standardDeck
+let shuffleDeck = function () {
+  // this loop will run through the amount of cards in standardDeck. (312)
+  for (let i = 0; i < unshuffledDeck.length; i++) {
+    // random number generator will assign random objects to the shuffled deck. (shuffling)
+    let shuffle = Math.floor(Math.random() * unshuffledDeck.length);
+    shuffledDeck.push(unshuffledDeck[shuffle]);
+  }
+};
+
+// 2.
+
 var main = function (input) {
-  var myOutputValue = "hello world";
-  return myOutputValue;
+  // game mode = "getUserInfo"
+  if (gameMode == "getUserInfo") {
+  }
+
+  let result = "hello world";
+  return result;
 };
